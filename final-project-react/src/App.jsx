41b8b1ee58375a01cebb6e5f8e5be41dccb1de50
CarlosJiZe <carlosjize@gmail.com>
2024-11-18 00:10:36 -0600

    cambios para deploy

--- final-project-react/src/App.jsx
+++ final-project-react/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import NavbarComponent from './Components/Navbar';
 import Home from './Components/Home';
 import Footer from './Components/Footer';
